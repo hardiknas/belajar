b755b7d14bfec9b2d47a6f3d7e29b1cf0093cc79
Hardiknas <i.hardiknas@gmail.com>
2024-12-14 23:48:33 +0800

    menambahkan file b.php

---
+++ b.php
@@ -0,0 +1,9 @@
+"Jangan Berhenti"
+
+Langkah kecil kutapaki pagi,
+Mentari tersenyum hangat di hati,
+Angin berbisik, "Jangan berhenti,"
+Mimpi tinggi kan kujangkau nanti,
+Di ujung jalan, ku temui arti
+Kuterus maju tanpa keraguan,
+Hingga cahaya membimbing tujuan.
